da0b816fadead6386873e7c00096146f587f8370
pathumboy247-cloud <pathumboy247@gmail.com>
2025-09-05 13:26:35 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "RIKA-XMD=nR8BHR5I#bjJymiKi6EiiXicbWkMsLGvBKDumFUakVhTU008rifQ", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "RIKA-XMD=OVlwFBxA#CsAzmFui8estEkfBoMBBaKW86cn99XBwS-K8u-MtVBA", // Add your session id
 PREFIX: process.env.PREFIX || ".",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
